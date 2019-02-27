11c7bce99544227de60e12b452fcfa82f2cbfdcd
Luis Alves <alvesgamadossantos@gmail.com>
2019-02-26 21:05:44 -0300

    feat(auth): Atenticar token para enviar nweet

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -4,7 +4,7 @@ const authConfig = require('../config/auth.json');
 
 function generateToken(params = {}){
       return jwt.sign(params, authConfig.secret, {
-            expiresIn: 60
+            expiresIn: 3600
       });
 }
 
--- routes/nweet.js
+++ routes/nweet.js
@@ -1,4 +1,8 @@
+const authMiddleware = require('../middlewares/auth')
+
 module.exports = function(app){
     var nweet = app.controllers.nweet;
-    app.post("/nweet/submit", nweet.submit);
+    app.post("/nweet/submit", authMiddleware,nweet.submit);
+    app.get("/nweet/listAll",nweet.listAll);
+
 }
